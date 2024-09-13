e95215360db5535685922bfede41a7ef591c4092
andriy-kolokolov <koloandre22@gmail.com>
2024-09-13 18:55:14 +0200

    setting up docker

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -2,12 +2,17 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run() : void
     {
+        if (User::count() >= 45) {
+            return;
+        }
+
         $this->call([
             UserSeeder::class,
         ]);
